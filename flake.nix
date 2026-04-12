03f5198e9ed3dd66a0b146f8953c8fca98e66543
den.aspects.pol <pol.dellaiera@protonmail.com>
2026-04-12 21:40:52 +0200

    chore: reorganise attributes logically

--- flake.nix
+++ flake.nix
@@ -4,29 +4,31 @@
   outputs =
     { self, ... }:
     let
-      allOutputs = publicOutputs // {
-        # Use explicit inherit, to ensure allOutputs is evaluated without
-        # evaluating devOutputs.
-        inherit (devOutputs) devShells checks formatter;
-      };
-
       # Currently none (`import flake-compat`)
       # TODO: add `lib`.
       publicOutputs = { };
 
-      devOutputs = devInputs.flake-parts.lib.mkFlake {
-        inputs = devInputs;
-      } ./dev/config.nix;
-      devInputs = devDeps // {
-        self = self // {
-          inputs = devInputs;
-        };
-      };
       # TODO: use clean library entrypoint when that's factored out.
       devDeps =
         (import ./default.nix {
           src = ./dev/deps;
         }).defaultNix.inputs;
+
+      devInputs = devDeps // {
+        self = self // {
+          inputs = devInputs;
+        };
+      };
+
+      devOutputs = devInputs.flake-parts.lib.mkFlake {
+        inputs = devInputs;
+      } ./dev/config.nix;
+
+      allOutputs = publicOutputs // {
+        # Use explicit inherit, to ensure allOutputs is evaluated without
+        # evaluating devOutputs.
+        inherit (devOutputs) devShells checks formatter;
+      };
     in
     allOutputs;
 }
